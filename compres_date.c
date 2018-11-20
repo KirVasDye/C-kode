5d74fe5e403800069ba8a0d4e6363c734f469613
KirVasDye <45197390+KirVasDye@users.noreply.github.com>
2018-11-20 20:10:04 +0300

    Update compres_date.c

--- compres_date.c
+++ compres_date.c
@@ -1,1 +1,54 @@
+#include"kurs.h"
+#include<stdio.h>
 
+
+int main(void) {
+
+	Date date;
+	datec_t datec;
+
+	// Ввод даты
+	printf("Enter date day is ");
+	scanf("%u", &date.dd);
+	printf("Enter date mounth is ");
+	scanf("%u", &date.mm);
+	printf("Enter date year is ");
+	scanf("%u", &date.yy);
+	datec = compressDate(date);
+
+	// Вывод даты
+
+	printDate(date);
+	printCompressedDate(datec);
+
+	// Установка нового дня
+
+	datec_t a;
+	printf("Enter new date day is ");
+	scanf("%u", &a);
+	datec = setDay(datec, a);
+	date = uncompressDate(datec);
+	printDate(date);
+	printCompressedDate(datec);
+
+	// Установка нового месяца
+
+	Month m;
+	printf("Enter new date mounth is ");
+	scanf("%u", &m);
+	datec = setMonth(datec, m);
+	date = uncompressDate(datec);
+	printDate(date);
+	printCompressedDate(datec);
+
+	// Установка нового года
+
+	printf("Enter new date year is ");
+	scanf("%u", &a);
+	datec = setYear(datec, a);
+	date = uncompressDate(datec);
+	printDate(date);
+	printCompressedDate(datec);
+
+	return 0;
+}
