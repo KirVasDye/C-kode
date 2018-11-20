282c761d0bd2cbc0ade9857dfb6f2acd58b0b167
KirVasDye <45197390+KirVasDye@users.noreply.github.com>
2018-11-20 23:40:53 +0300

    Create binar.h

---
+++ binar.h
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<math.h>
+
+unsigned short digitBinar(unsigned int a) {
+	unsigned short res = 0;
+	while (a > 0) {
+		res++;
+		a /= 2;
+	}
+	return res;
+}
+
+unsigned int shifrBinar(unsigned int a) {
+	unsigned int buf = a;
+	unsigned int res = 0;
+	int i, sup = 1;
+	for (i = 0; i < digitBinar(a); i += 2) {
+		res += ((buf % 2) * 2 + (buf / 2) % 2)*sup;
+		sup *= 4;
+	}
+	return res;
+}
+
+void printAsBinar(unsigned int a) {
+	for (int i = sizeof(unsigned int) * 8 - 1; i >= 0; i--) {
+		unsigned int mask = (1 << i);
+		if (a & mask)
+			printf("1");
+		else
+			printf("0");
+	}
+	printf("\n");
+}
+
+unsigned int sdvigRBinar(unsigned int a, int n) {
+	unsigned int buf;
+	int i;
+	for (i = 0; i < n; i++) {
+		buf = (a % 2)<< (sizeof(unsigned int) * 8 - 1);
+		a >>= 1;
+		a |= buf;
+	}
+	return a;
+}
