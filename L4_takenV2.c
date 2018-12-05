2a1e98be30a7ce951b955d11aa48ae4d232c0134
KirVasDye <45197390+KirVasDye@users.noreply.github.com>
2018-12-05 20:49:30 +0300

    Update L4_takenV2.c

--- L4_takenV2.c
+++ L4_takenV2.c
@@ -14,42 +14,45 @@ int main(void) {
 		arr[0][1] = tmp;
 	}
 	printArr(arr, n);
-	printf("You can use 'w','a','s','d' to movment\n");
-	findZero(arr, n, i, j);
+	printf("\nYou can use 'w','a','s','d' to movment\n");
+	findZero(arr, n, &i, &j);
 	char move;
 	while (!final(arr, n)) {
 		move = getchar();
 		switch (move) {
 		case 'w':
 			if (upR(i, n)) {
-				up(arr, i, j);
+				up(arr, &i, &j);
+				printArr(arr, n);
 			}
 			else
 				printf(ERROR);
 			break;
 		case 's':
 			if (downR(i, n)) {
-				down(arr, i, j);
+				down(arr, &i, &j);
+				printArr(arr, n);
 			}
 			else
 				printf(ERROR);
 			break;
 		case 'd':
 			if (rightR(j, n)) {
-				right(arr, i, j);
+				right(arr, &i, &j);
+				printArr(arr, n);
 			}
 			else
 				printf(ERROR);
 			break;
 		case 'a':
 			if (leftR(j, n)) {
-				left(arr, i, j);
+				left(arr, &i, &j);
+				printArr(arr, n);
 			}
 			else
 				printf(ERROR);
 			break;
 		}
-		printArr(arr, n);
 	}
 
 	for (i = 0; i < n; i++)
